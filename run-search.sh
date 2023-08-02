95299a5dc1738a7362aca7bba713c7b5c39d43ba
Fabio MOREIRA <fnmbr6hpink8f66p@561.jp>
2023-08-02 21:59:54 +0900

    Add aarch64 interpreter

--- run-search.sh
+++ run-search.sh
@@ -5,4 +5,4 @@ set +eu
 CORES=$(grep -c ^processor /proc/cpuinfo)
 
 rm -f output/*.tmp
-seq $((16#0000)) $((16#FFFF)) | gargs -p $((CORES - 1)) "nice -n 20 lua5.2 generate.lua {0}"
+seq $((16#0000)) $((16#FFFF)) | gargs -p $((CORES - 1)) "nice -n 20 bin/lua generate.lua {0}"
